2ccdc6eaa87ede44590187b2df5ce8cee3defafd
Harley Laue <harley.laue@billups.com>
2017-10-07 02:54:24 -0700

    remove some keys from adjust, make LSHT a OSM, & change lower to have mute & pgup/pgdn

--- keyboards/nyquist/keymaps/workman/keymap.c
+++ keyboards/nyquist/keymaps/workman/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
+  OSM(MOD_LSFT), KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -124,17 +124,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------||------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  ||  F6  |   _  |   +  |     |    \  |  |   |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 ||  F12 |ISO ~ |ISO | |      |      |Enter |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 ||  F12 | PgUp | PgDn |      |      |Enter |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
- * |      |      |      |      |      |      ||      |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      ||      |      | Mute | Vol- | Vol+ |      |
  * `-----------------------------------------'`-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGUP, KC_PGDN, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______ \
 ),
 
 /* Raise
@@ -160,22 +160,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------..-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  ||  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * | RESET|      |      |      |      |      ||      |      |      |      |      |      |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
- * |      | Reset|RGB Tg|RGB Md|Hue Up|Hue Dn||Sat Up|Sat Dn|Val Up|Val Dn|      |  Del |
+ * |      |  F1  |  F2  |  F3  |  F4  |      ||      |      |      |      |      |      |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm||AGswap|Qwerty|Colmak|Workmn|Dvorak|      |
+ * |      |  F5  |  F6  |  F7  |  F8  |      |||Qwerty|Colmak|Workmn|Dvorak|     |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
- * |      |      |      |      |      |      ||      |      |      |      |      |      |
+ * |      |  F9  |  F10 |  F11 |  F12 |      ||      |      |      |      |      |      |
  * |------+------+------+------+------+------||------+------+------+------+------+------|
  * |      |      |      |      |      |      ||      |      |      |      |      |      |
  * `-----------------------------------------''-----------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_DEL, \
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, WORKMAN, DVORAK,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   RESET , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______,\
+  _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______, _______, QWERTY,  COLEMAK, WORKMAN, DVORAK,  _______, \
+  _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 
