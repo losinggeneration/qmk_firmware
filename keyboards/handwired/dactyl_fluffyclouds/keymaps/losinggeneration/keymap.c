9b2659854f9f8a151e4e21569e23c0ebf2a8791c
Harley Laue <harley.laue@billups.com>
2020-03-12 12:24:17 -0700

    Fix tilda/grave placement

--- keyboards/handwired/dactyl_fluffyclouds/keymaps/losinggeneration/keymap.c
+++ keyboards/handwired/dactyl_fluffyclouds/keymaps/losinggeneration/keymap.c
@@ -6,7 +6,7 @@
 extern keymap_config_t keymap_config;
 
 #define NUMBER_ROW \
-  KC_TILD ,KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV
+  KC_GRV  ,KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_TILD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
