ced8efdbb7239404c69486b252b81f2f9d7e96c3
Harley Laue <losinggeneration@gmail.com>
2021-07-25 05:06:30 -0700

    Some documentation for xd75

--- keyboards/xd75/keymaps/losinggeneration/keymap.c
+++ keyboards/xd75/keymaps/losinggeneration/keymap.c
@@ -15,6 +15,7 @@
  */
 #include "losinggeneration.h"
 
+/* Take a 5x12 layout & put a 5x3 in the middle to get 5x15 */
 #define ortho_5x12_to_5x15(                                   \
   K00, K01, K02, K03, K04, K05, K09, K0A, K0B, K0C, K0D, K0E, \
   K10, K11, K12, K13, K14, K15, K19, K1A, K1B, K1C, K1D, K1E, \
@@ -33,15 +34,30 @@
   K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, \
   K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E  \
 
+/* The basic layout doesn't expand macros in parameters, this works around that */
 #define ortho_5x12_to_5x15_expand(...) ortho_5x12_to_5x15(__VA_ARGS__)
 
 #define NUMBER_ROW \
   KC_GRV  ,KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_DEL
 
+/* The basic layout doesn't expand macros in parameters, this works around that */
 #define LAYOUT_ortho_5x15_expand(...) LAYOUT_ortho_5x15(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/*
+ * ┌──────┬──────┬──────┐
+ * │   -  │   `  │   =  │
+ * ├──────┼──────┼──────┤
+ * │  [   │   \  │   ]  │
+ * ├──────┼──────┼──────┤
+ * │ Home │  Del │ PgUp │
+ * ├──────┼──────┼──────┤
+ * │ End  │  Up  │ PgDn │
+ * ├──────┼──────┼──────┤
+ * │      │      │      │
+ * └──────┴──────┴──────┘
+ */
 #define ortho_5x3_default \
   KC_MINS, KC_GRV , KC_EQL , \
   KC_LBRC, KC_BSLS, KC_RBRC, \
