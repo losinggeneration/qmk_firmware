fe82b6541056c27f2f9fda3a24423d4a545787a3
Harley Laue <losinggeneration@gmail.com>
2022-04-19 12:38:00 -0700

    Keyboard is fixed, back to master left

--- keyboards/keebio/levinson/keymaps/losinggeneration/config.h
+++ keyboards/keebio/levinson/keymaps/losinggeneration/config.h
@@ -25,8 +25,8 @@ long with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-/* #define MASTER_LEFT */
-#define MASTER_RIGHT
+#define MASTER_LEFT
+/* #define MASTER_RIGHT */
 /* #define EE_HANDS */
 
 #define MIDI_BASIC
